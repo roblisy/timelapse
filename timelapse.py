6d62fe3f1e341f34fbdf49e00d47bb91b4970dc7
Rob Lisy <roblisy@gmail.com>
2024-12-02 20:29:53 -0800

    Updating readme and changing code for timelapse

--- timelapse.py
+++ timelapse.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import sys
 import yaml
 import glob
 from datetime import datetime
@@ -10,6 +11,16 @@ from google.cloud import storage
 with open('config.yml', 'r') as file:
     config = yaml.safe_load(file)
 
+def set_photo_quality():
+    """
+    Uses gphoto2 settings specific to my camera model to set the image size and quality.
+    We don't really need super high quality images, since they will be stacked on top of
+    each other and processed into a timelapse movie.
+    """
+    subprocess.run(["gphoto2", "--set-config", "imagequality=0"]) # Quality standard
+    subprocess.run(["gphoto2", "--set-config", "imagesize=2"]) # Size small
+    subprocess.run(["gphoto2", "--set-config", "expprogram=15"]) # Landscape exposure program
+    
 def capture_image(filename: str):
     """Function to call gphoto2 and download the image locally to the Raspberry Pi"""
     subprocess.run(["gphoto2", "--capture-image-and-download", "--filename", filename])
@@ -19,23 +30,36 @@ def upload_image(filename: str):
     storage_client = storage.Client()
     bucket = storage_client.bucket(config['bucket_name'])
     blob = bucket.blob(filename)
-    blob.upload_from_filename(filename)
+    blob.upload_from_filename(filename, timeout=config['upload_timeout'])
 
 if __name__ == "__main__":
+    set_photo_quality()
     while True:
         # Create the file name
         filename = f"""{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.jpg"""
         
         # Call gphoto and take the picture
         capture_image(filename=filename)
-        upload_image(filename=filename)
+        # Upload, with shitty error handling.
+        print(f"Uploading original file {filename}")
+        try:
+            upload_image(filename=filename)
+        except:
+            print("There was an error")
+
+        #jpg_files = glob.glob(f"{filename}.jpg")
+        # for file in jpg_files:
+        #    print(f"Uploading {file}")
+        #    upload_image(filename=file)
+        #    os.remove(file)
+        #upload_image(filename=filename)
 
         # Look for any existing local jpgs and upload them
         # As is this will OVERWRITE any duplicate files in the bucket with the same name
-        jpg_files = glob.glob("*.jpg")
-        for file in jpg_files:
-            upload_image(file)
-            print(f"removing {file}")
-            os.remove(file)
+        #jpg_files = glob.glob("*.jpg")
+        #for file in jpg_files:
+        #    upload_image(file)
+        #    print(f"removing {file}")
+        #    os.remove(file)
         # Wait for the interval and do it all again
         sleep(config['interval'])
